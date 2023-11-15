85c81d5c47f7a105f7a0f307ccbc725d050a0473
Brayan-Castro <castro081828@gmail.com>
2023-11-14 21:35:47 -0300

    added functionality: user can now choose the rest and study time

--- pomodoro.py
+++ pomodoro.py
@@ -1,26 +1,38 @@
 import time
 import pyautogui as alrt
-import sys
 
+print("*********** Welcome to my Pomodoro Timer! ***********")
+
+print("")
+ 
+try:
+    stTime = int(input("How much study time (default = 50min)? "))
+    rsTime = int(input("How much resting time (default = 10min)?"))
+except ValueError:
+    print("Error: input isn't a number, please try again")
+    exit()
+
+print("")
 usrInput = input("Do you want to start the timer? (y/n) ")
 
 def holder():
     base = int(time.time())
-    pomodoroTimer(base)
+    pomodoroTimer(base, stTime, rsTime)
 
-def pomodoroTimer(base):
+def pomodoroTimer(base, study = 50, rest = 10):
     restTimer = 0
     while True:
         timer = int(time.time())
 
-        if (timer == base + 60):
+        if (timer == base + (study * 60)):
             alrt.alert(text="time to rest!!", title="Pomodoro")
             restTimer = int(time.time())
-        elif (timer == restTimer + 30):
+        elif (timer == restTimer + (rest * 60)):
             alrt.alert(text="time to study!!", title="Pomodoro")
             base = int(time.time())
 
 if usrInput == 'y':
+    print("Timer is running!")
     holder()
 else:
     exit()
