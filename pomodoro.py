527cbe12fed8a9087f795eebaa40a9ce690c4f93
Brayan-Castro <castro081828@gmail.com>
2023-11-14 21:09:41 -0300

    v1.0, functional with crude UI

---
+++ pomodoro.py
@@ -0,0 +1,26 @@
+import time
+import pyautogui as alrt
+import sys
+
+usrInput = input("Do you want to start the timer? (y/n) ")
+
+def holder():
+    base = int(time.time())
+    pomodoroTimer(base)
+
+def pomodoroTimer(base):
+    restTimer = 0
+    while True:
+        timer = int(time.time())
+
+        if (timer == base + 60):
+            alrt.alert(text="time to rest!!", title="Pomodoro")
+            restTimer = int(time.time())
+        elif (timer == restTimer + 30):
+            alrt.alert(text="time to study!!", title="Pomodoro")
+            base = int(time.time())
+
+if usrInput == 'y':
+    holder()
+else:
+    exit()
